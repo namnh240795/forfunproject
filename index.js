ec9e5e8510f90cbc83efde484d7a741ba488ca2c
NamNguyen <admin@Admins-MacBook-Pro.local>
2017-12-21 15:57:27 +0700

    add login api

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = require('./config');
+// import PORT from './config';
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/forfun', (req, res) => res.json({ok: true}));
+app.post('/login', (req, res) => {
+    const { body } = req;
+    if(body.username !== 'admin')
+        return res.json({ error: 'User not found'});
+    else {
+        if(body.password == '1234567890' ) {
+            return res.json({ token: 'kasjdfkljaslkdjflkasjdflkj' });
+        } else return res.json({ error: 'Wrong password' });
+    }
+});
 
-app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || PORT, () => console.log(`Example app listening on port ${PORT}!`));
